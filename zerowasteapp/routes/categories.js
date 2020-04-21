b2b9dbbca0ea8818f66c409994603f24a987365a
Amberpl <mateusz.amb@gmail.com>
2020-04-21 23:43:24 +0200

    Shops

--- zerowasteapp/routes/categories.js
+++ zerowasteapp/routes/categories.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const Event = require('../models/event');
+const Address = require('../models/address');
+const Shop = require('../models/shops');
 const passport = require('passport');
 const multer = require('multer');
 const fs = require('fs');
@@ -44,7 +46,7 @@ router.post('/events', upload.single('eventImage'), passport.authenticate('jwt',
                 msg: 'Event created'
             })
         }
-    })
+    });
 });
 
 router.put('/events/:_id/update', upload.single('eventImage'), passport.authenticate('jwt', {session: false}), (req, res, next) => {
@@ -178,8 +180,55 @@ router.get('/events/filter/eventDescription/:filter', (req, res, next) => {
                 events: events
             })
         }
-    })
+    });
 });
 
+// Address
+router.post('/newAddress', (req, res, next) => {
+    let newAddress = new Address({
+        street: req.body.street,
+        buildingNumber: req.body.buildingNumber,
+        apartmentNumber: req.body.apartmentNumber,
+        postCode: req.body.postCode,
+        cityName: req.body.cityName
+    });
+
+    Address.addAddress(newAddress, (err, address) => {
+        if(err){
+            res.json({success: false, msg: 'Failed to add new address.'})
+        }else {
+            res.json({success: true, msg: 'New address added.'})
+        }
+    });
+});
+
+// Shops
+router.post('/newShop', (req, res) => {
+    let newShop = new Shop({
+        name: req.body.name,
+        shopAddress: req.body.shopAddress,
+        email: req.body.email,
+        phoneNumber: req.body.phoneNumber,
+        rating: req.body.rating,
+        createAt: req.body.createAt,
+        uploadAt: req.body.uploadAt,
+        idUser: req.user,
+        enable: req.body.enabled
+    });
+
+    Shop.addShop(newShop, (err, shop) =>{
+        if (err) {
+            res.json({
+                success: false,
+                msg: 'Failed to add new shop.'
+            })
+        } else {
+            res.json({
+                success: true,
+                msg: 'Shop Added.'
+            })
+        }
+    })
+});
 
 module.exports = router;
