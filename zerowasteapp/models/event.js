44faad35058d0a205f2f11e308866fbaeafa7198
Julia Hardy <julia.joanna.hardy@gmail.com>
2020-04-22 21:55:22 +0200

    fix registration

--- zerowasteapp/models/event.js
+++ zerowasteapp/models/event.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const UserSchema = require('../models/user').UserSchema;
+const UserSchema = mongoose.model('User').schema;
 
 const EventSchema = mongoose.Schema({
     name: {
