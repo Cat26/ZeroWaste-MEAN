bfffe98d78f173b521d2562c6290a01e8b986af4
Julia Hardy <julia.joanna.hardy@gmail.com>
2020-04-28 22:51:20 +0200

    get user id to sign up for event

--- zerowaste-frontend/src/app/components/events/events-list/event-item/event-item.component.ts
+++ zerowaste-frontend/src/app/components/events/events-list/event-item/event-item.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit, Input } from '@angular/core';
 import * as moment from 'moment';
 import { AuthService } from '../../../../services/auth/auth.service';
+import { EventsService } from "../../../../services/events/events.service";
+import { FlashMessagesService } from "angular2-flash-messages";
 
 @Component({
   selector: 'app-event-item',
@@ -8,24 +10,58 @@ import { AuthService } from '../../../../services/auth/auth.service';
   styleUrls: ['./event-item.component.css']
 })
 export class EventItemComponent implements OnInit {
+  loggedInUserId: string;
   @Input() eventItem: {
     name: string,
     description: string,
     eventImage: string,
     eventDate: string,
-    likesUserList: [],
-    dislikesUserList: [],
-    participants: [],
+    likesUserList: string[],
+    dislikesUserList: string[],
+    participants: string[],
     owner: { username: string },
     _id: string
   };
 
   constructor(
-    public authService: AuthService
+    public authService: AuthService,
+    private eventService: EventsService,
+    private flashMessage: FlashMessagesService
   ) {
   }
 
   ngOnInit(): void {
+  }
+
+  getLoggedUserId() {
+    this.authService.loadUser();
+    let user = JSON.parse(this.authService.user);
+    this.loggedInUserId = this.authService.loggedIn() ? user.id : null;
+  }
+
+  checkUserIdInParticipantsList () {
+    this.getLoggedUserId();
+    return this.eventItem.participants.includes(this.loggedInUserId);
+  }
+
+  signInToEvent () {
+    let participantsList = [this.loggedInUserId].concat(this.eventItem.participants);
+    // this.eventService.updateEvent(this.eventItem._id, {}).subscribe(
+    //   (res: any) => {
+    //     if (res.success) {
+    //       this.flashMessage.show(
+    //         res.msg,
+    //         {cssClass: 'alert-success', timeout: 3000}
+    //       );
+    //     } else {
+    //       this.flashMessage.show(
+    //         res.msg,
+    //         {cssClass: 'alert-danger', timeout: 5000}
+    //       );
+    //       console.log(res.error)
+    //     }
+    //   }
+    // )
   }
 
   formatDate(date) {
