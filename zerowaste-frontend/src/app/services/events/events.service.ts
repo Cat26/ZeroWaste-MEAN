01793bedec3564052a8670a53ae5aa77ef8c3575
Julia Hardy <julia.joanna.hardy@gmail.com>
2020-05-15 22:22:14 +0200

    filter and sort events backend

--- zerowaste-frontend/src/app/services/events/events.service.ts
+++ zerowaste-frontend/src/app/services/events/events.service.ts
@@ -62,4 +62,49 @@ export class EventsService {
     };
     return this.http.get('http://localhost:3000/categories/newest', httpOptions);
   }
+
+  sortEventsByDateDescending() {
+    const httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json'
+      })
+    };
+    return this.http.get('http://localhost:3000/categories/events/sort-desc', httpOptions);
+  }
+
+  sortEventsByDateAscending() {
+    const httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json'
+      })
+    };
+    return this.http.get('http://localhost:3000/categories/events/sort-asc', httpOptions);
+  }
+
+  filterEventsByOwner(filterQuery) {
+    const httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json'
+      })
+    };
+    return this.http.get('http://localhost:3000/categories/events/filter/owner/' + filterQuery, httpOptions);
+  }
+
+  filterEventsByName(filterQuery) {
+    const httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json'
+      })
+    };
+    return this.http.get('http://localhost:3000/categories/events/filter/eventName/' + filterQuery, httpOptions);
+  }
+
+  filterEventsByDescription(filterQuery) {
+    const httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json'
+      })
+    };
+    return this.http.get('http://localhost:3000/categories/events/filter/eventDescription/' + filterQuery, httpOptions);
+  }
 }
