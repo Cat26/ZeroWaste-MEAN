8c7f13ad101b1f5d87639f4755bddb25deb0cfde
Julia Hardy <julia.joanna.hardy@gmail.com>
2020-06-12 21:43:42 +0200

    tests implementation

--- zerowaste-frontend/src/app/components/events/events-list/event-item/event-item.component.spec.ts
+++ zerowaste-frontend/src/app/components/events/events-list/event-item/event-item.component.spec.ts
@@ -1,14 +1,17 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { FlashMessagesModule } from "angular2-flash-messages";
 
 import { EventItemComponent } from './event-item.component';
 
-describe('EventItemComponent', () => {
+fdescribe('EventItemComponent', () => {
   let component: EventItemComponent;
   let fixture: ComponentFixture<EventItemComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ EventItemComponent ]
+      declarations: [ EventItemComponent ],
+      imports: [HttpClientTestingModule, FlashMessagesModule.forRoot()]
     })
     .compileComponents();
   }));
@@ -22,4 +25,12 @@ describe('EventItemComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  describe('formatDate', function () {
+    it('should format given date to full format (example: Monday, May 12, 2020 3:29 PM)',
+      function () {
+      let date = new Date(2020,5,12,3,24,0);
+      expect(component.formatDate(date)).toEqual('Friday, June 12, 2020 3:24 AM');
+    })
+  })
 });
--- zerowaste-frontend/src/app/components/profile/user-calendar/user-calendar.component.spec.ts
+++ zerowaste-frontend/src/app/components/profile/user-calendar/user-calendar.component.spec.ts
@@ -22,4 +22,11 @@ describe('UserCalendarComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  // describe('getHourAndMinutes function', function () {
+  //   it('should modify given date string and return date in format hours:minutes', function() {
+  //     let dateString = new Date('December 17, 1995 03:24:00').toDateString();
+  //     expect(component.getHourAndMinutes(dateString)).toEqual('3:24');
+  //   })
+  // });
 });
